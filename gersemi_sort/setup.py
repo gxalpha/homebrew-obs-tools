453f3b74ea9db6b466f8351454238a29c1cc8eb8
Sebastian Beckmann <beckmann.sebastian@outlook.de>
2025-12-21 16:41:02 +0100

    Add gersemi_sort
    Closes #1.
    
    Signed-off-by: Sebastian Beckmann <59806498+gxalpha@users.noreply.github.com>

---
+++ gersemi_sort/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gersemi_sort',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    description="TODO",
+    license='MPL-2.0',
+    python_requires='>=3.6',
+)
+
